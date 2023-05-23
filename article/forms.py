9bd4c5d6cd12086ad82526c1c3959d63ba40fb62
amine <aminetmtc@hotmail.com>
2023-05-23 19:44:39 +0100

    validationerror et petites retouches

--- article/forms.py
+++ article/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-
+from django.core.exceptions import ValidationError
 
 class SurveyForm(forms.Form):
     name = forms.CharField(max_length=30)
@@ -44,8 +44,28 @@ class SurveyForm(forms.Form):
         choices=yes_no_choices, widget=forms.RadioSelect)
 
     # add clean_<fieldname>() method to clean and validate form fields, for example :
-    # def clean_name(self):
-    #     name = self.cleaned_data.get('name')
-    #     if not name:
-    #         raise forms.ValidationError("name is required")
-    #     return name
+    def clean_name(self):
+         name = self.cleaned_data.get('name')
+         if not name:
+             raise forms.ValidationError("nom manquant")
+         return name
+    def clean_fname(self):
+         fname = self.cleaned_data.get('fname')
+         if not fname:
+             raise forms.ValidationError("prenom manquant")
+         return fname
+    def clean_age(self):
+         age = self.cleaned_data.get('age')
+         if not age:
+             raise forms.ValidationError("age manquant")
+         return age
+    def clean_phone(self):
+         phone_number = self.cleaned_data.get('phone_number')
+         if not phone_number:
+             raise forms.ValidationError("telephone manquant")
+         return phone_number
+    def clean_gender(self):
+         gender = self.cleaned_data.get('gender')
+         if not gender:
+             raise forms.ValidationError("specifier le sexe")
+         return gender
